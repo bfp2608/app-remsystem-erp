37f3c27e98086aaf80210f1332a792af538de432
bfp2608 <betofelipe2608@gmail.com>
2026-01-14 09:32:46 -0500

    Initial commit: React + Spring Boot project

---
+++ backend/src/test/java/com/remsystem/api_erp/ApiErpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.remsystem.api_erp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiErpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
