00464644fe36c54646e7a914cd52e0ead1c3f1e8
bfp2608 <betofelipe2608@gmail.com>
2026-01-22 11:09:11 -0500

    microservicio para formulario de crear usuarios.

---
+++ backend/API-AdminForm/src/main/java/admin/form/ApiAdminFormApplication.java
@@ -0,0 +1,13 @@
+package admin.form;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAdminFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAdminFormApplication.class, args);
+	}
+
+}
