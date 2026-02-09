95b940b499569b9b827151a34d539f0620dc9d99
bfp2608 <betofelipe2608@gmail.com>
2026-02-09 11:56:59 -0500

    Reforma del LOGIN para remsystem sistema.

---
+++ backend/login-remsystem-api/src/main/java/remsystem/login/LoginRemsystemApplication.java
@@ -0,0 +1,13 @@
+package remsystem.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginRemsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginRemsystemApplication.class, args);
+	}
+
+}
---
+++ backend/login-remsystem-api/src/test/java/remsystem/login/LoginRemsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package remsystem.login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginRemsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
