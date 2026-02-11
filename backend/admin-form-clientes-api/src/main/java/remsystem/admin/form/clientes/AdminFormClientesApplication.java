751cdba4018c9f185b0dc8f25ee9bdd75a309b3a
bfp2608 <betofelipe2608@gmail.com>
2026-02-11 12:05:21 -0500

    Inicio del API para gestionar Clientes.

--- backend/admin-form-clientes-api/src/main/java/remsystem/admin/form/clientes/AdminFormClientesApplication.java
+++ backend/admin-form-clientes-api/src/main/java/remsystem/admin/form/clientes/AdminFormClientesApplication.java
@@ -2,8 +2,10 @@ package remsystem.admin.form.clientes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class AdminFormClientesApplication {
 
 	public static void main(String[] args) {
